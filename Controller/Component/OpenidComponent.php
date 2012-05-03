8890fc7f73ee83bd69c21342c272cdc72939d404
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2012-05-03 15:36:27 +0200

    Adding Auth_Yadis_CakeSession (issue #4)

--- Controller/Component/OpenidComponent.php
+++ Controller/Component/OpenidComponent.php
@@ -34,9 +34,6 @@ class OpenidComponent extends Component {
         parent::__construct($collection, $settings);
         $this->handleSettings($settings);
 
-        // XXX ensure the session is started to avoid "Undefined variable _SESSION" notices from the OpenID lib
-        session_start();
-
         $pathToVendorsFolder = $this->getPathToVendorsFolderWithOpenIDLibrary();
 
         if ($pathToVendorsFolder == '') {
@@ -149,7 +146,7 @@ class OpenidComponent extends Component {
     }
 
     private function getConsumer() {
-        $consumer = new Auth_OpenID_Consumer($this->getStore());
+        $consumer = new Auth_OpenID_Consumer($this->getStore(), new Auth_Yadis_CakeSession());
 
         if ($this->acceptGoogleApps) {
             new GApps_OpenID_Discovery($consumer);
@@ -304,5 +301,60 @@ class OpenidComponent extends Component {
              "<body onload='document.getElementById(\"".$formId."\").submit()'>".
              $formHtml.'</body></html>';
         exit;
+    }
+}
+
+/**
+ * Yadis CakeSession Handler, implements the "interface" defined by Auth_Yadis_PHPSession in Vendor/Auth/Yadis/Manager.php
+ *
+ * Since cake has its own implementation of sessions, we route directly through
+ * the CakeSession class so as to avoid unexpected errors.
+ */
+class Auth_Yadis_CakeSession {
+    private $prefix = 'Yadis';
+
+    /**
+     * Set a session key/value pair.
+     *
+     * @param string $name The name of the session key to add.
+     * @param string $value The value to add to the session.
+     */
+    public function set($name, $value) {
+        CakeSession::write($this->prefix . '.' . $name, $value);
+    }
+
+    /**
+     * Get a key's value from the session.
+     *
+     * @param string $name The name of the key to retrieve.
+     * @param string $default The optional value to return if the key
+     * is not found in the session.
+     * @return string $result The key's value in the session or
+     * $default if it isn't found.
+     */
+    public function get($name, $default = null) {
+        $value = CakeSession::read($this->prefix . '.' . $name);
+
+        if ($value !== null) {
+            return $value;
+        } else {
+            return $default;
+        }
+    }
+
+    /**
+     * Remove a key/value pair from the session.
+     *
+     * @param string $name The name of the key to remove.
+     */
+    public function del($name) {
+        CakeSession::delete($this->prefix . '.' . $name);
+    }
+
+    /**
+     * Return the contents of the session in array form.
+     */
+    public function contents() {
+        return CakeSession::read($this->prefix);
     }
 }
